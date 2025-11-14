e1b9451f55837aa2d63a0c43dcd8603ff2cd3317
Grawikos <mar.grawinski@gmail.com>
2025-11-14 20:19:45 +0100

    Initial React app for OpenShift

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "30px", fontSize: "20px" }}>
+      <h2>React App Running on OpenShift</h2>
+      {/* <p>Backend endpoint: {process.env.REACT_APP_API_URL}</p> */}
+    </div>
+  );
+}
+
+export default App;
