df1d0a75cc2af98e0348017756af81946cc6c8fd
Grawikos <mar.grawinski@gmail.com>
2025-11-15 13:30:03 +0100

    test webhook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 function App() {
-  const backendUrl = process.env.REACT_APP_BACKEND_URL || "Backend URL not set\n use {oc set env dc/react-frontend REACT_APP_BACKEND_URL='http://my-backend-route-url.com'}";
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || "Backend URL not set, use \n\
+oc set env deploy/react-frontend REACT_APP_BACKEND_URL='http://my-backend-route-url.com'\n\
+oc start-build bc/react-frontend --follow";
 
   return (
     <div style={{ padding: "30px", fontSize: "20px" }}>
-      <h2>React App Running on OpenShift</h2>
+      <h2>React App Running on OpenShift version 2</h2>
       <p>Backend API URL: <strong>{backendUrl}</strong></p>
+      <p>TEST WEBHOOK3</p>
     </div>
   );
 }
