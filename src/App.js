a969d85bed2e4c065ef63cce66ab0ad62dcd849a
Grawikos <mar.grawinski@gmail.com>
2025-11-15 13:07:49 +0100

    backend link test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 function App() {
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || "Backend URL not set\n use {oc set env dc/react-frontend REACT_APP_BACKEND_URL='http://my-backend-route-url.com'}";
+
   return (
     <div style={{ padding: "30px", fontSize: "20px" }}>
       <h2>React App Running on OpenShift</h2>
-      {/* <p>Backend endpoint: {process.env.REACT_APP_API_URL}</p> */}
+      <p>Backend API URL: <strong>{backendUrl}</strong></p>
     </div>
   );
 }
